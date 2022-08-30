abf54bf0f128fdabafbbcbba896e6d3f7e73851a
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-08-29 23:51:39 -0400

    Added loot tables for fire, ice and poison crystal

--- src/main/java/com/massinissadjellouli/RPGmod/block/ModBlocks.java
+++ src/main/java/com/massinissadjellouli/RPGmod/block/ModBlocks.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -25,27 +26,27 @@ public class ModBlocks {
     //Crystals
     public static final RegistryObject<Block> FIRE_CRYSTAL_ORE = registerBlock("fire_crystal_ore",
             ()-> new DropExperienceBlock(
-            BlockBehaviour.Properties.of(Material.STONE).strength(7.6f).requiresCorrectToolForDrops(),
+            BlockBehaviour.Properties.of(Material.STONE).strength(14f).requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)),CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> DEEPSLATE_FIRE_CRYSTAL_ORE = registerBlock("deepslate_fire_crystal_ore",
             ()-> new DropExperienceBlock(
-            BlockBehaviour.Properties.of(Material.STONE).strength(7.6f).requiresCorrectToolForDrops(),
+            BlockBehaviour.Properties.of(Material.STONE).strength(14f).requiresCorrectToolForDrops(),
         UniformInt.of(3,7)),CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> ICE_CRYSTAL_ORE = registerBlock("ice_crystal_ore",
             ()-> new DropExperienceBlock(
-            BlockBehaviour.Properties.of(Material.STONE).strength(7.6f).requiresCorrectToolForDrops(),
+            BlockBehaviour.Properties.of(Material.STONE).strength(14f).requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)),CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> DEEPSLATE_ICE_CRYSTAL_ORE = registerBlock("deepslate_ice_crystal_ore",
             ()-> new DropExperienceBlock(
-            BlockBehaviour.Properties.of(Material.STONE).strength(7.6f).requiresCorrectToolForDrops(),
+            BlockBehaviour.Properties.of(Material.STONE).strength(14f).requiresCorrectToolForDrops(),
         UniformInt.of(3,7)),CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> POISON_CRYSTAL_ORE = registerBlock("poison_crystal_ore",
             ()-> new DropExperienceBlock(
-            BlockBehaviour.Properties.of(Material.STONE).strength(7.6f).requiresCorrectToolForDrops(),
+            BlockBehaviour.Properties.of(Material.STONE).strength(14f).requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)),CreativeModeTab.TAB_BUILDING_BLOCKS);
     public static final RegistryObject<Block> DEEPSLATE_POISON_CRYSTAL_ORE = registerBlock("deepslate_poison_crystal_ore",
             ()-> new DropExperienceBlock(
-            BlockBehaviour.Properties.of(Material.STONE).strength(7.6f).requiresCorrectToolForDrops(),
+            BlockBehaviour.Properties.of(Material.STONE).strength(14f).requiresCorrectToolForDrops(),
         UniformInt.of(3,7)),CreativeModeTab.TAB_BUILDING_BLOCKS);
 
     private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab){
