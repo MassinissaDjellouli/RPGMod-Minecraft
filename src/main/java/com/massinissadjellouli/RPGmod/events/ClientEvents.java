8cfc6a4932faee1930a60a0e02e03b0eeb4a3d62
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-09-03 23:59:20 -0400

    Rarity system implemented. Only used to say item rarity in tooltip curently.

--- src/main/java/com/massinissadjellouli/RPGmod/events/ClientEvents.java
+++ src/main/java/com/massinissadjellouli/RPGmod/events/ClientEvents.java
@@ -2,22 +2,30 @@ package com.massinissadjellouli.RPGmod.events;
 
 import com.massinissadjellouli.RPGmod.RPGMod;
 import com.massinissadjellouli.RPGmod.client.ClientGamemodeData;
-import com.massinissadjellouli.RPGmod.client.ClientThirstData;
 import com.massinissadjellouli.RPGmod.client.ThirstHudOverlay;
 import com.massinissadjellouli.RPGmod.networking.ModPackets;
 import com.massinissadjellouli.RPGmod.networking.packet.*;
+import com.massinissadjellouli.RPGmod.tags.ModTags;
 import com.massinissadjellouli.RPGmod.thirst.PlayerThirst;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.tags.ITagManager;
+import org.apache.commons.lang3.text.WordUtils;
 
 @Mod.EventBusSubscriber(modid = RPGMod.MODID,value = Dist.CLIENT)
 public class ClientEvents {
@@ -46,6 +54,23 @@ public class ClientEvents {
             ModPackets.sendToServer(new ThirstEffectC2SPacket());
 
         }
+    }
+
+    @SubscribeEvent
+    public static void itemTooltip(ItemTooltipEvent event){
+        String tooltip = "";
+        ITagManager<Item> tagManager = ForgeRegistries.ITEMS.tags();
+        Item item = event.getItemStack().getItem();
+        ChatFormatting style = ChatFormatting.WHITE;
+        for (ModTags.Items.RarityTags tagKey : ModTags.Items.RarityTags.values()){
+            style = tagKey.style;
+            TagKey<Item> tag = tagKey.tagKey;
+            if(tagManager.getTag(tag).contains(item)){
+                tooltip = tagKey.name;
+                break;
+            }
+        }
+        event.getToolTip().add(Component.literal(tooltip).withStyle(style));
     }
 
     @SubscribeEvent
--- src/main/java/com/massinissadjellouli/RPGmod/tags/ModTags.java
+++ src/main/java/com/massinissadjellouli/RPGmod/tags/ModTags.java
@@ -1,9 +1,12 @@
 package com.massinissadjellouli.RPGmod.tags;
 
 import com.massinissadjellouli.RPGmod.RPGMod;
+import net.minecraft.ChatFormatting;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class ModTags {
@@ -16,6 +19,37 @@ public class ModTags {
     }
     public static class Items{
 
+        public enum RarityTags {
+            COMMON(ModTags.Items.COMMON,"Commun"),
+            UNCOMMON(ModTags.Items.UNCOMMON,"Peu commun",ChatFormatting.GREEN),
+            RARE(ModTags.Items.RARE,"Rare",ChatFormatting.BLUE),
+            VERY_RARE(ModTags.Items.VERY_RARE,"Très rare",ChatFormatting.DARK_PURPLE),
+            LEGENDARY(ModTags.Items.LEGENDARY,"Légendaire",ChatFormatting.GOLD),
+            MYTHICAL(ModTags.Items.MYTHICAL,"Mythique",ChatFormatting.RED);
+
+            public final TagKey<Item> tagKey;
+            public final String name;
+            public ChatFormatting style = ChatFormatting.WHITE;
+            RarityTags(TagKey<Item> rarity,String name,ChatFormatting style) {
+                tagKey = rarity;
+                this.name = name;
+                this.style = style;
+            }
+            RarityTags(TagKey<Item> rarity,String name) {
+                tagKey = rarity;
+                this.name = name;
+            }
+        }
+
+        public static final TagKey<Item> COMMON = createTag("rarity/common");
+        public static final TagKey<Item> UNCOMMON = createTag("rarity/uncommon");
+        public static final TagKey<Item> RARE = createTag("rarity/rare");
+        public static final TagKey<Item> VERY_RARE = createTag("rarity/very_rare");
+        public static final TagKey<Item> LEGENDARY = createTag("rarity/legendary");
+        public static final TagKey<Item> MYTHICAL = createTag("rarity/mythical");
+        private static TagKey<Item> createTag(String ressourceLocation){
+            return ItemTags.create(new ResourceLocation(RPGMod.MODID,ressourceLocation));
+        }
     }
 
 
