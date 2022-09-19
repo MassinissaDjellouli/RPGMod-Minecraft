316c635c3bea375ef4394343b09c325498609bc5
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-09-19 00:39:45 -0400

    Added a static class to store the last message received

---
+++ src/main/java/com/massinissadjellouli/RPGmod/client/ClientLastMessageReceived.java
@@ -0,0 +1,29 @@
+package com.massinissadjellouli.RPGmod.client;
+
+public class ClientLastMessageReceived {
+    private static String lastMessage = "";
+    private static int ticksSinceLastMessage;
+    private static final int TICKS_BEFORE_DELETE = 100;
+    public static String get() {
+        return lastMessage;
+    }
+
+    public static void set(String lastMessage) {
+        ticksSinceLastMessage = 0;
+        ClientLastMessageReceived.lastMessage = lastMessage;
+    }
+
+    public static boolean isLastMessageEmpty(){
+        return lastMessage.isEmpty();
+    }
+    public static void incrementTick(){
+        if(isLastMessageEmpty()){
+            return;
+        }
+        ticksSinceLastMessage++;
+        if(ticksSinceLastMessage >= TICKS_BEFORE_DELETE){
+            ticksSinceLastMessage = 0;
+            lastMessage = "";
+        }
+    }
+}
