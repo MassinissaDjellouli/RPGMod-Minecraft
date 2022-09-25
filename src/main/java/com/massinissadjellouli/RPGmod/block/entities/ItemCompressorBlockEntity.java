d2eca8259e965869b72aaa292546582c65b84755
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-09-25 02:34:03 -0400

    Created item compressor block entity but still have to implement functionality

---
+++ src/main/java/com/massinissadjellouli/RPGmod/block/entities/ItemCompressorBlockEntity.java
@@ -0,0 +1,183 @@
+package com.massinissadjellouli.RPGmod.block.entities;
+
+import com.massinissadjellouli.RPGmod.item.ModItems;
+import com.massinissadjellouli.RPGmod.screen.ItemCompressorMenu;
+import com.massinissadjellouli.RPGmod.tags.ModTags;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.Containers;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ItemCompressorBlockEntity extends BlockEntity implements MenuProvider {
+
+    public static final int AMOUNT_OF_SLOTS_TO_COMPRESS_ROWS = 3;
+    public static final int AMOUNT_OF_SLOTS_TO_COMPRESS_COLS = 3;
+    public static final int AMOUNT_OF_SLOTS_TO_COMPRESS = AMOUNT_OF_SLOTS_TO_COMPRESS_ROWS * AMOUNT_OF_SLOTS_TO_COMPRESS_COLS;
+    public static final int POSITION_OF_RESULT_SLOT = 10;
+    public static final int AMOUNT_OF_SLOTS = 10;
+    private int progress;
+    protected final ContainerData data;
+    private int maxProgress = 200;
+    public ItemCompressorBlockEntity(BlockPos p_155229_, BlockState p_155230_) {
+        super(ModBlockEntities.ITEM_COMPRESSOR.get(), p_155229_, p_155230_);
+        data = new ContainerData() {
+            @Override
+            public int get(int index) {
+                return switch (index){
+                    case 0 -> ItemCompressorBlockEntity.this.progress;
+                    case 1 -> ItemCompressorBlockEntity.this.maxProgress;
+                    default -> 0;
+                };
+            }
+
+            @Override
+            public void set(int index, int value) {
+                switch (index){
+                    case 0 -> ItemCompressorBlockEntity.this.progress = value;
+                    case 1 -> ItemCompressorBlockEntity.this.maxProgress = value;
+                };
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        };
+    }
+    private final ItemStackHandler itemStackHandler = new ItemStackHandler(AMOUNT_OF_SLOTS){
+        @Override
+        protected void onContentsChanged(int slot){
+            setChanged();
+        }
+    };
+
+
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+
+
+    @Override
+    public Component getDisplayName() {
+        return Component.literal("Compresseur a item");
+    }
+
+    @Nullable
+    @Override
+    public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+        return new ItemCompressorMenu(id,inventory,this,this.data);
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @NotNull Direction side) {
+        if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY){
+            return lazyItemHandler.cast();
+        }
+        return super.getCapability(cap,side);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        lazyItemHandler = LazyOptional.of(()->itemStackHandler);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        lazyItemHandler.invalidate();
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag nbt) {
+        nbt.put("inventory",itemStackHandler.serializeNBT());
+        super.saveAdditional(nbt);
+    }
+
+    @Override
+    public void load(CompoundTag nbt) {
+        super.load(nbt);
+        itemStackHandler.deserializeNBT(nbt.getCompound("inventory"));
+    }
+
+    public void drops(){
+        SimpleContainer inventory = new SimpleContainer(itemStackHandler.getSlots());
+        for (int i = 0; i < itemStackHandler.getSlots(); i++) {
+            inventory.setItem(i,itemStackHandler.getStackInSlot(i));
+        }
+        Containers.dropContents(this.level,this.worldPosition,inventory);
+    }
+
+    public static <E extends BlockEntity> void tick(Level level, BlockPos blockPos,
+                                                    BlockState state, ItemCompressorBlockEntity itemCompressor) {
+
+        if(level.isClientSide()) return;
+        if(hasRecipe(itemCompressor)){
+            itemCompressor.progress++;
+            setChanged(level,blockPos,state);
+            if(itemCompressor.progress >= itemCompressor.maxProgress){
+                craftItem(itemCompressor);
+            }{
+                itemCompressor.resetProgress();
+                setChanged(level,blockPos,state);
+            }
+        }
+    }
+
+    private static boolean hasRecipe(ItemCompressorBlockEntity itemCompressor) {
+        SimpleContainer inventory = new SimpleContainer(itemCompressor.itemStackHandler.getSlots());
+        for (int i = 0; i < itemCompressor.itemStackHandler.getSlots(); i++) {
+            inventory.setItem(i,itemCompressor.itemStackHandler.getStackInSlot(i));
+        }
+
+        boolean isAcceptedItemInCompressorSlots = true;
+        for (int i = 0; i < AMOUNT_OF_SLOTS_TO_COMPRESS; i++) {
+            isAcceptedItemInCompressorSlots =
+                    isAcceptedItemInCompressorSlots &&
+                    ForgeRegistries.ITEMS.tags().getTag(
+                        ModTags.Items.ACCEPTED_ITEMS_IN_COMPRESSOR_SLOTS).contains(
+                                itemCompressor.itemStackHandler.getStackInSlot(i).getItem());
+        }
+
+        return isAcceptedItemInCompressorSlots && canInsertAmountIntoResultSlot(inventory) && canInsertItemIntoOutputSlot(inventory,new ItemStack(ModItems.COPPER_AXE.get(),1));
+    }
+
+    private static boolean canInsertAmountIntoResultSlot(SimpleContainer inventory) {
+        return inventory.getItem(POSITION_OF_RESULT_SLOT).getMaxStackSize() > inventory.getItem(POSITION_OF_RESULT_SLOT).getCount();
+    }
+
+    private static boolean canInsertItemIntoOutputSlot(SimpleContainer inventory, ItemStack itemStack) {
+        return inventory.getItem(POSITION_OF_RESULT_SLOT).getItem() == itemStack.getItem() || inventory.getItem(POSITION_OF_RESULT_SLOT).isEmpty();
+    }
+
+    private void resetProgress() {
+        this.progress = 0;
+    }
+
+    private static void craftItem(ItemCompressorBlockEntity itemCompressor) {
+        if(hasRecipe(itemCompressor)){
+            //TODO: cheeck with 9 slots
+            itemCompressor.itemStackHandler.extractItem(1,1,false);
+            itemCompressor.itemStackHandler.setStackInSlot(POSITION_OF_RESULT_SLOT,new ItemStack(ModItems.COPPER_AXE.get(),
+                    itemCompressor.itemStackHandler.getStackInSlot(POSITION_OF_RESULT_SLOT).getCount() + 1));
+            itemCompressor.resetProgress();
+        }
+    }
+}
--- src/main/java/com/massinissadjellouli/RPGmod/block/entities/ModBlockEntities.java
+++ src/main/java/com/massinissadjellouli/RPGmod/block/entities/ModBlockEntities.java
@@ -1,15 +1,22 @@
 package com.massinissadjellouli.RPGmod.block.entities;
 
 import com.massinissadjellouli.RPGmod.RPGMod;
+import com.massinissadjellouli.RPGmod.block.ModBlocks;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPE_DEFERRED_REGISTER =
         DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, RPGMod.MODID);
 
+    public static final RegistryObject<BlockEntityType<ItemCompressorBlockEntity>> ITEM_COMPRESSOR =
+            BLOCK_ENTITY_TYPE_DEFERRED_REGISTER.register("item_compressor",
+                    ()->
+                BlockEntityType.Builder.of(ItemCompressorBlockEntity::new,
+                        ModBlocks.ITEM_COMPRESSOR.get()).build(null));
     public static void registerBlockEntities(IEventBus eventBus){
         BLOCK_ENTITY_TYPE_DEFERRED_REGISTER.register(eventBus);
     }
