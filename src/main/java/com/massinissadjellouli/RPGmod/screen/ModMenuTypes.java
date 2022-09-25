a1080183b2df1cc98acabd4ba2f52b9eba75428a
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-09-25 02:33:13 -0400

    Moved ModMenuTypes

---
+++ src/main/java/com/massinissadjellouli/RPGmod/screen/ModMenuTypes.java
@@ -0,0 +1,27 @@
+package com.massinissadjellouli.RPGmod.screen;
+
+import com.massinissadjellouli.RPGmod.RPGMod;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.common.extensions.IForgeMenuType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.network.IContainerFactory;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModMenuTypes {
+
+    public static final DeferredRegister<MenuType<?>> MENU_TYPE_DEFERRED_REGISTER =
+            DeferredRegister.create(ForgeRegistries.MENU_TYPES, RPGMod.MODID);
+
+    public static final RegistryObject<MenuType<ItemCompressorMenu>> ITEM_COMPRESSOR_MENU =
+            registerMenuType(ItemCompressorMenu::new,"item_compressor_menu");
+    private static <T extends AbstractContainerMenu>RegistryObject<MenuType<T>> registerMenuType(
+            IContainerFactory<T> factory, String name){
+        return MENU_TYPE_DEFERRED_REGISTER.register(name,()-> IForgeMenuType.create(factory));
+    }
+    public static void registerMenus(IEventBus eventBus){
+        MENU_TYPE_DEFERRED_REGISTER.register(eventBus);
+    }
+}
