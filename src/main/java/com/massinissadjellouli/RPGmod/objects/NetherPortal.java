8edaa2a3c8a6929cc4e72bbfc9f05f3cbdb3f3b1
Massinissa Djellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-11-06 20:40:57 -0500

    Added nether spread from portal

--- src/main/java/com/massinissadjellouli/RPGmod/objects/NetherPortal.java
+++ src/main/java/com/massinissadjellouli/RPGmod/objects/NetherPortal.java
@@ -1,37 +1,244 @@
 package com.massinissadjellouli.RPGmod.objects;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.EmptyFluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.phys.AABB;
+
+import java.util.List;
+import java.util.Random;
 
 public class NetherPortal {
     private BlockPos portalStart;
     private BlockPos portalEnd;
     private BlockPos portalFrameStart;
     private BlockPos portalFrameEnd;
-
+    private boolean isSpreading;
+    private int spreadRadius;
+    private int spreadProgress;
+    private final static int SPREAD_RADIUS_MAX = 30;
+    private final static int SPREAD_HEIGHT_MAX = 10;
+    private final static int SPREAD_PROGRESS_DURATION = 20;
     public NetherPortal(BlockPos portalStart, BlockPos portalEnd) {
         this.portalStart = portalStart;
         this.portalEnd = portalEnd;
         this.portalFrameStart = getPortalFrameStart();
         this.portalFrameEnd = getPortalFrameEnd();
+        this.isSpreading = false;
+        this.spreadRadius = 0;
     }
 
     private BlockPos getPortalFrameEnd() {
+        return switch (direction()){
+            case "Z" -> new BlockPos(portalEnd.getX(),portalEnd.getY() + 1,portalEnd.getZ() + 1);
+            case "X" -> new BlockPos(portalEnd.getX() + 1,portalEnd.getY() + 1,portalEnd.getZ());
+            default -> null;
+        };
+    }
+
+    private BlockPos getPortalFrameStart() {
+        return switch (direction()){
+            case "Z" -> new BlockPos(portalStart.getX(),portalStart.getY() - 1,portalStart.getZ() - 1);
+            case "X" -> new BlockPos(portalStart.getX() - 1,portalStart.getY() - 1,portalStart.getZ());
+            default -> null;
+            };
+    }
+
+    public String direction(){
         if(portalStart.getX() == portalEnd.getX()){
-            return new BlockPos(portalEnd.getX(),portalEnd.getY() + 1,portalEnd.getZ() + 1);
+            return "Z";
         }
         if(portalStart.getZ() == portalEnd.getZ()){
-            return new BlockPos(portalEnd.getX() + 1,portalEnd.getY() + 1,portalEnd.getZ());
+            return "X";
         }
         return null;
     }
 
-    private BlockPos getPortalFrameStart() {
-        if(portalStart.getX() == portalEnd.getX()){
-            return new BlockPos(portalStart.getX(),portalStart.getY() - 1,portalStart.getZ() - 1);
+    public int size(){
+        return length() * height();
+    }
+
+    public int length(){
+        return switch (direction()) {
+            case "X" -> portalEnd.getX() - portalStart.getX();
+            case "Z" -> portalEnd.getZ() - portalStart.getZ();
+            default -> 0;
+        };
+    }
+
+    public int height(){
+     return portalEnd.getY() - portalStart.getY();
+    }
+    public void startSpread(){
+        this.isSpreading = true;
+    }
+
+    public void end(){
+        this.isSpreading = false;
+        this.spreadRadius = 0;
+    }
+    public void tick(ServerLevel level){
+        if (!isSpreading){
+            return;
         }
-        if(portalStart.getZ() == portalEnd.getZ()){
-            return new BlockPos(portalStart.getX() - 1,portalStart.getY() - 1,portalStart.getZ());
+        if(spreadProgress < SPREAD_PROGRESS_DURATION){
+            spreadProgress++;
+            return;
         }
-        return null;
+        if(spreadRadius < SPREAD_RADIUS_MAX){
+            spreadRadius++;
+            spreadProgress = 0;
+            spread(level);
+            return;
+        }
+        end();
+    }
+
+    private void spread(ServerLevel level) {
+        if(middle() == null){
+            return;
+        }
+        BlockPos beginingBP = middle()
+                .above(Math.min(spreadRadius, SPREAD_HEIGHT_MAX))
+                .north(spreadRadius)
+                .west(spreadRadius);
+        BlockPos endBP = middle()
+                .below(Math.min(spreadRadius, SPREAD_HEIGHT_MAX))
+                .south(spreadRadius)
+                .east(spreadRadius);
+        BlockPos.betweenClosedStream(new AABB(beginingBP, endBP))
+                .filter(blockPos -> {
+                    Block block = level.getBlockState(blockPos).getBlock();
+                    return isSpreadableBlock(block);
+                })
+                .forEach(blockPosition -> {
+                    if (isAir(level.getBlockState(blockPosition).getBlock())){
+                        if (isSameBlock(level.getBlockState(blockPosition.below()).getBlock(), Blocks.NETHERRACK)) {
+                            level.setBlockAndUpdate(blockPosition, Blocks.FIRE.defaultBlockState());
+                        }
+                        return;
+                    }
+                    level.setBlockAndUpdate(blockPosition, switch (new Random().nextInt(15)){
+                        case 1 -> Blocks.SOUL_SOIL.defaultBlockState();
+                        case 2,3 -> Blocks.SOUL_SAND.defaultBlockState();
+                        case 4,5 -> Blocks.MAGMA_BLOCK.defaultBlockState();
+                        case 6,7,8 -> Blocks.NETHER_BRICKS.defaultBlockState();
+                        default -> Blocks.NETHERRACK.defaultBlockState();
+                    });
+                });
+    }
+
+    private boolean isSameBlock(Block block1, Block block2){
+        return block1.getName().equals(block2.getName());
+    }
+
+    private boolean isAir(Block block){
+        return isSameBlock(block, Blocks.AIR);
+    }
+    private boolean isSpreadableBlock(Block block) {
+        if(isAir(block) && new Random().nextInt(100) == 1){
+            return true;
+        }
+        for (Block unspreadableBlock : unspreadableBlocks()) {
+            if (isSameBlock(block, unspreadableBlock)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private BlockPos middle() {
+        if (portalFrameStart == null || portalFrameEnd == null) {
+            return null;
+        }
+        return switch (direction()){
+            case "X" -> new BlockPos(
+                    portalFrameStart.getX() + (portalFrameEnd.getX() - portalFrameStart.getX()) / 2
+                    ,portalFrameStart.getY(),
+                    portalFrameStart.getZ());
+            case "Z" -> new BlockPos(
+                    portalFrameStart.getX(),
+                    portalFrameStart.getY(),
+                    portalFrameStart.getZ() + (portalFrameEnd.getZ() - portalFrameStart.getZ()) / 2);
+            default -> null;
+        };
+    }
+
+    private List<Block> unspreadableBlocks(){
+        return List.of(
+                Blocks.OBSIDIAN,
+                Blocks.AIR,
+                Blocks.CAVE_AIR,
+                Blocks.VOID_AIR,
+                Blocks.BEDROCK,
+                Blocks.NETHER_PORTAL,
+                Blocks.END_PORTAL,
+                Blocks.END_PORTAL_FRAME,
+                Blocks.END_GATEWAY,
+                Blocks.NETHERRACK,
+                Blocks.NETHER_BRICKS,
+                Blocks.NETHER_GOLD_ORE,
+                Blocks.NETHER_QUARTZ_ORE,
+                Blocks.NETHER_WART_BLOCK,
+                Blocks.NETHER_SPROUTS,
+                Blocks.NETHER_BRICK_FENCE,
+                Blocks.NETHER_BRICK_SLAB,
+                Blocks.NETHER_BRICK_STAIRS,
+                Blocks.NETHER_BRICK_WALL,
+                Blocks.NETHER_WART_BLOCK,
+                Blocks.NETHER_GOLD_ORE,
+                Blocks.SOUL_CAMPFIRE,
+                Blocks.SOUL_FIRE,
+                Blocks.SOUL_LANTERN,
+                Blocks.SOUL_SAND,
+                Blocks.SOUL_SOIL,
+                Blocks.SOUL_TORCH,
+                Blocks.SOUL_WALL_TORCH,
+                Blocks.WATER,
+                Blocks.LAVA,
+                Blocks.FIRE,
+                Blocks.CRYING_OBSIDIAN,
+                Blocks.BUBBLE_COLUMN,
+                Blocks.CRIMSON_BUTTON,
+                Blocks.CRIMSON_DOOR,
+                Blocks.CRIMSON_FENCE,
+                Blocks.CRIMSON_FENCE_GATE,
+                Blocks.CRIMSON_PRESSURE_PLATE,
+                Blocks.CRIMSON_SIGN,
+                Blocks.CRIMSON_TRAPDOOR,
+                Blocks.CRIMSON_WALL_SIGN,
+                Blocks.CRIMSON_FUNGUS,
+                Blocks.CRIMSON_HYPHAE,
+                Blocks.CRIMSON_NYLIUM,
+                Blocks.CRIMSON_PLANKS,
+                Blocks.CRIMSON_ROOTS,
+                Blocks.CRIMSON_SLAB,
+                Blocks.CRIMSON_STAIRS,
+                Blocks.CRIMSON_STEM,
+                Blocks.WARPED_BUTTON,
+                Blocks.WARPED_DOOR,
+                Blocks.WARPED_FENCE,
+                Blocks.WARPED_FENCE_GATE,
+                Blocks.WARPED_PRESSURE_PLATE,
+                Blocks.WARPED_SIGN,
+                Blocks.WARPED_TRAPDOOR,
+                Blocks.WARPED_WALL_SIGN,
+                Blocks.WARPED_WART_BLOCK,
+                Blocks.WARPED_ROOTS,
+                Blocks.WARPED_STEM,
+                Blocks.WARPED_HYPHAE,
+                Blocks.WARPED_NYLIUM,
+                Blocks.WARPED_PLANKS,
+                Blocks.WARPED_SLAB,
+                Blocks.WARPED_STAIRS,
+                Blocks.WARPED_FUNGUS,
+                Blocks.MAGMA_BLOCK
+        );
+
     }
 }
--- src/main/java/com/massinissadjellouli/RPGmod/worldEvents/InvasionNetherWorldEvent.java
+++ src/main/java/com/massinissadjellouli/RPGmod/worldEvents/InvasionNetherWorldEvent.java
@@ -9,6 +9,7 @@ import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.border.WorldBorder;
 import net.minecraft.world.phys.AABB;
 
 import java.time.LocalDateTime;
@@ -20,6 +21,8 @@ import java.util.stream.Stream;
 import static net.minecraft.core.BlockPos.withinManhattanStream;
 
 public class InvasionNetherWorldEvent extends WorldEvent {
+    List<NetherPortal> netherPortals = new ArrayList<>();
+
     public InvasionNetherWorldEvent() {
         showTitle = true;
     }
@@ -29,34 +32,38 @@ public class InvasionNetherWorldEvent extends WorldEvent {
         ClientLastTitleReceived.set("Attention! Un event invasion du nether commence!",
                 "Éloignez vous des portails du nether!",
                 ChatFormatting.DARK_RED);
-        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
         List<BlockPos> blockPos = netherPortalsInRangeOfPlayer(200);
-        convertBlockPosListToNetherPortals(blockPos);
-        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")));
-        System.out.println("Invasion Nether World Event launched");
+        netherPortals = convertBlockPosListToNetherPortals(blockPos);
+        netherPortals.forEach(NetherPortal::startSpread);
     }
 
-    private void convertBlockPosListToNetherPortals(List<BlockPos> blockPos) {
-        Map<Integer,List<BlockPos>> xMap = new HashMap<>();
-        Map<Integer,List<BlockPos>> zMap = new HashMap<>();
+    @Override
+    public void end() {
+        netherPortals.forEach(NetherPortal::end);
+        super.end();
+    }
+
+    private List<NetherPortal> convertBlockPosListToNetherPortals(List<BlockPos> blockPos) {
+        Map<Integer, List<BlockPos>> xMap = new HashMap<>();
+        Map<Integer, List<BlockPos>> zMap = new HashMap<>();
         Set<Integer> zCoordsRemovedFromX = new HashSet<>();
         for (BlockPos pos : blockPos) {
-            addToOrInitMap(xMap,pos.getX(),pos);
-            addToOrInitMap(zMap,pos.getZ(),pos);
+            addToOrInitMap(xMap, pos.getX(), pos);
+            addToOrInitMap(zMap, pos.getZ(), pos);
         }
         xMap.forEach((x, bp) -> {
             ArrayList<BlockPos> filteredX = new ArrayList<>();
             bp.forEach(blockPosition -> {
-                if(filteredX.stream().map(Vec3i::getZ).toList().contains(blockPosition.getZ())){
+                if (filteredX.stream().map(Vec3i::getZ).toList().contains(blockPosition.getZ())) {
                     return;
                 }
                 filteredX.add(blockPosition);
             });
-            xMap.put(x,filteredX);
+            xMap.put(x, filteredX);
         });
         ArrayList<Integer> xKeysToRemove = new ArrayList<>();
         xMap.forEach((x, bp) -> {
-            if(bp.size() == 1){
+            if (bp.size() == 1) {
                 zCoordsRemovedFromX.add(bp.get(0).getZ());
                 xKeysToRemove.add(x);
             }
@@ -64,7 +71,7 @@ public class InvasionNetherWorldEvent extends WorldEvent {
         xKeysToRemove.forEach(xMap::remove);
         ArrayList<Integer> zKeysToRemove = new ArrayList<>();
         zMap.forEach((z, bp) -> {
-            if(!zCoordsRemovedFromX.contains(z)){
+            if (!zCoordsRemovedFromX.contains(z)) {
                 zKeysToRemove.add(z);
             }
         });
@@ -74,57 +81,65 @@ public class InvasionNetherWorldEvent extends WorldEvent {
             int maxZ = bp.stream().max(Comparator.comparingInt(Vec3i::getZ)).get().getZ();
             int minZ = bp.stream().min(Comparator.comparingInt(Vec3i::getZ)).get().getZ();
 
-            Stream<BlockPos> blockPosStream = blockPos.stream().filter(blockPosition -> blockPosition.getZ() == maxZ && blockPosition.getX() == x
-            );
-            int minY = blockPosStream.min(Comparator.comparingInt(Vec3i::getY)).get().getY();
-            int maxY = blockPosStream.max(Comparator.comparingInt(Vec3i::getY)).get().getY();
-            netherPortals.add(new NetherPortal(new BlockPos(x,minY,minZ),new BlockPos(x,maxY,maxZ)));
+            int minY = blockPos.stream().filter(
+                    blockPosition -> blockPosition.getZ() == maxZ && blockPosition.getX() == x
+            ).min(Comparator.comparingInt(Vec3i::getY)).get().getY();
+
+            int maxY = blockPos.stream().filter(
+                    blockPosition -> blockPosition.getZ() == maxZ && blockPosition.getX() == x
+            ).max(Comparator.comparingInt(Vec3i::getY)).get().getY();
+
+            netherPortals.add(new NetherPortal(new BlockPos(x, minY, minZ), new BlockPos(x, maxY, maxZ)));
         });
         zMap.forEach((z, bp) -> {
             int maxX = bp.stream().max(Comparator.comparingInt(Vec3i::getX)).get().getX();
             int minX = bp.stream().min(Comparator.comparingInt(Vec3i::getX)).get().getX();
 
-            Stream<BlockPos> blockPosStream = blockPos.stream().filter(blockPosition -> blockPosition.getX() == maxX && blockPosition.getZ() == z
-            );
-            int minY = blockPosStream.min(Comparator.comparingInt(Vec3i::getY)).get().getY();
-            int maxY = blockPosStream.max(Comparator.comparingInt(Vec3i::getY)).get().getY();
-            netherPortals.add(new NetherPortal(new BlockPos(minX,minY,z),new BlockPos(maxX,maxY,z)));
+            ;
+            int minY = blockPos.stream().filter(
+                    blockPosition -> blockPosition.getX() == maxX && blockPosition.getZ() == z
+            ).min(Comparator.comparingInt(Vec3i::getY)).get().getY();
+            int maxY = blockPos.stream().filter(blockPosition -> blockPosition.getX() == maxX && blockPosition.getZ() == z
+            ).max(Comparator.comparingInt(Vec3i::getY)).get().getY();
+            netherPortals.add(new NetherPortal(new BlockPos(minX, minY, z), new BlockPos(maxX, maxY, z)));
         });
-        System.out.println("xMap: " + xMap);
+        return netherPortals;
     }
 
-    private <K,V> void addToOrInitMap(Map<K,List<V>> map, K key, V value) {
+    private <K, V> void addToOrInitMap(Map<K, List<V>> map, K key, V value) {
         if (map.containsKey(key)) {
             map.get(key).add(value);
         } else {
             List<V> list = new ArrayList<>();
             list.add(value);
-            map.put(key,list);
+            map.put(key, list);
         }
     }
+
     @Override
     protected void tick() {
-
+        netherPortals.forEach(netherPortal -> {
+            netherPortal.tick(level);
+        });
     }
 
     @Override
     protected int[] getEventRange() {
-        return new int[]{2000,4000};
+        return new int[]{2000, 4000};
     }
 
-    private List<BlockPos> netherPortalsInRangeOfPlayer(int range){
-        if(range > 350){
+    private List<BlockPos> netherPortalsInRangeOfPlayer(int range) {
+        if (range > 350) {
             throw new IllegalArgumentException("Range is too big.");
         }
         final int SEARCH_HEIGHT = 20;
         BlockPos blockPos = player.blockPosition();
-
         BlockPos beginingBP = blockPos.above(SEARCH_HEIGHT).east(range).south(range);
         BlockPos endBP = blockPos.below(SEARCH_HEIGHT).west(range).north(range);
         ArrayList<BlockPos> portals = new ArrayList<>();
-        BlockPos.betweenClosedStream(new AABB(beginingBP,endBP)).forEach(
+        BlockPos.betweenClosedStream(new AABB(beginingBP, endBP)).forEach(
                 blockPosition -> {
-                    if(level.getBlockState(blockPosition).getBlock().getName().equals(Blocks.NETHER_PORTAL.getName())){
+                    if (level.getBlockState(blockPosition).getBlock().getName().equals(Blocks.NETHER_PORTAL.getName())) {
                         portals.add(new BlockPos(blockPosition));
                     }
                 });
